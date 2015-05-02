357c003a9e83402d7899ff5a1be164aa30f77826
Scott Zellers <scott_zellers@brown.edu>
2015-05-02 16:40:01 -0400

    stopped people from adding tiles to places they aren't allowed

--- src/main/java/edu/brown/cs/scij/network/CarcBackEnd.java
+++ src/main/java/edu/brown/cs/scij/network/CarcBackEnd.java
@@ -131,6 +131,28 @@ public class CarcBackEnd implements BackEnd {
           s.putField("error", "That wasn't a valid move! Please try again");
           toReturn.put("error", "That wasn't a valid move! Please try again");
           return toReturn;
+        } catch (IllegalArgumentException e) {
+          s.putField("board", r.getBoard());
+          toReturn.put("board", r.getBoard());
+          s.putField("currentPlayer", r.getCurPlayer());
+          toReturn.put("currentPlayer", r.getCurPlayer());
+          s.putField("players", r.getPlayers());
+          toReturn.put("players", r.getPlayers());
+
+          s.putField("gameOver", r.isGameOver());
+          toReturn.put("gameOver", r.isGameOver());
+          s.putField("currTile", r.getCurTile());
+          toReturn.put("currTile", r.getCurTile());
+          validMoves = r.getBoard().validMoves(r.getCurTile());
+          s.putField("validMoves", validMoves);
+          toReturn.put("validMoves", validMoves);
+          s.putField("validMeeples", new ArrayList<>());
+          toReturn.put("validMeeples", new ArrayList<>());
+          s.putField("tilesLeft", r.getDeck().getTiles().size());
+          toReturn.put("tilesLeft", r.getDeck().getTiles().size());
+          s.putField("error", "That wasn't a valid move! Please try again");
+          toReturn.put("error", "That wasn't a valid move! Please try again");
+          return toReturn;
         }
 
         // putField: board, current player, list of all players, valid meeples,
