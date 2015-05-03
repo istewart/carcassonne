fcaf52297f642754725c7303ab957d6d8eb34266
Joseph Bellavia <joseph_bellavia@brown.edu>
2015-05-03 17:43:28 -0400

    fixed compiling issue caused by an unknown party

--- src/main/java/edu/brown/cs/scij/network/Network.java
+++ src/main/java/edu/brown/cs/scij/network/Network.java
@@ -101,7 +101,7 @@ public final class Network {
     try {
       ip = InetAddress.getLocalHost().getHostAddress().toString();
       url = "http://" + ip + ":" + Integer.toString(port) + "/home";
-      System.out.println("The network is loading...")
+      System.out.println("The network is loading...");
       System.out.println("If a window does not open automatically, please visit " + url);
       server.putField("url", url);
     } catch (Exception ex) {
